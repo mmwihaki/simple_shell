e00ce8585d3594035be9f22596101d59c726d01f
mmwihaki <marymwihaki16@gmail.com>
2023-07-26 00:58:12 -0700

    handle error

---
+++ handle_error.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "shell.h"
+/**
+ * handle_error - handle erros
+ * @program_name: program name
+ * @error_message: error message
+ * description - handle error
+ * Return: void
+ */
+void handle_error(const char *program_name, const char *error_message)
+{
+	fprintf(stderr, "%s: %s\n", program_name, error_massage);
+}
--- shell.h
+++ shell.h
@@ -7,7 +7,7 @@ char *read_user_input();
 char *parse_command(char *input);
 char *search_executable(char *command);
 void execute_command(char *command);
-void handle_error(const char *error_messsage);
+void handle_error(const char *program_name, const char *error_messsage);
 void shell_loop(void);
 
 #endif
